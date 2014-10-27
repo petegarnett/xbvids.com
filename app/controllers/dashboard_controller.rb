61408b75a8eb5e41f2df3ca621eb30d1410493bd
Nick Jones <nick@nicksays.co.uk>
2014-10-27 22:38:21 +0000

    Add gamertag changing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,25 @@
 class DashboardController < ApplicationController
 
-   before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def index
+    if current_user.gamertag.nil?
+      redirect_to settings_path
+      return
+    end
+
     @videos = current_user.videos
+  end
+
+  def settings
+
+  end
+
+  def settings_post
+    gamertag = params[:gamertag]
+
+    current_user.update_attributes(:gamertag => gamertag)
+
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
 
+  get 'settings' => 'dashboard#settings'
+  post 'settings' => 'dashboard#settings_post', as: 'settings_post'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
