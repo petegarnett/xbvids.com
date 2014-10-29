cf43a0663e12b94deb52ed4b867ae91d36f341e7
Nick Jones <nick@nicksays.co.uk>
2014-10-29 22:26:37 +0000

    Fix syntax

--- app/workers/upload_thumbnail_worker.rb
+++ app/workers/upload_thumbnail_worker.rb
@@ -9,17 +9,16 @@ class UploadThumbnailWorker
 
     video = Video.find_by_clip_id video_clip_id
 
-      thumbnail_file_name = '%s.png' % [video.clip_id]
-      thumbnail_location = '/tmp/%s' % [thumbnail_file_name]
+    thumbnail_file_name = '%s.png' % [video.clip_id]
+    thumbnail_location = '/tmp/%s' % [thumbnail_file_name]
 
-      puts "Downloading %s to %s" % [video.clip_id, thumbnail_location]
-      open(thumbnail_location, 'wb') do |file|
-        file << open(video.thumbnail_large).read
-      end
-
-      puts "Uploading %s" % [thumbnail_location]
-
-      s3.buckets[bucket_name].objects[thumbnail_file_name].write(:file => thumbnail_location, :acl => :public_read)
+    puts "Downloading %s to %s" % [video.clip_id, thumbnail_location]
+    open(thumbnail_location, 'wb') do |file|
+      file << open(video.thumbnail_large).read
     end
+
+    puts "Uploading %s" % [thumbnail_location]
+
+    s3.buckets[bucket_name].objects[thumbnail_file_name].write(:file => thumbnail_location, :acl => :public_read)
   end
 end
