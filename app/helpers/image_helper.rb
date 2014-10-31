8ea895f28eba8f3be794e26a9d0c7fb1850ce281
Nick Jones <nick@nicksays.co.uk>
2014-10-31 23:50:38 +0000

    Ensure there's a source tag to stop the white dot appearing

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -10,4 +10,8 @@ module ImageHelper
 
     return ("https://i.embed.ly/1/image/resize?%s" % [api_params.to_query(nil)]).html_safe
   end
+
+  def self.transparent_gif
+    return 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='
+  end
 end
