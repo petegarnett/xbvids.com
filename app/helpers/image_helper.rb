6368da2e5b24019278491119152ffe43b688e993
Nick Jones <nick@nicksays.co.uk>
2014-11-14 16:27:29 +0000

    Revert "Use own image proxy"
    This reverts commit a9173bab2155e27e06bf2176ea536cd93a83f45a.

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,13 +1,14 @@
 module ImageHelper
   def self.resized(url, width, height=nil)
     api_params = {
-      :image   => url,
+      :url   => url,
       :width => width,
+      :key   => '3b8da2efff024d37856e11ab0878dfa9'
     }
 
     api_params[:height] = height unless height.nil?
 
-    return ("https://img.xbvids.com?%s" % [api_params.to_query(nil)]).html_safe
+    return ("https://i.embed.ly/1/image/resize?%s" % [api_params.to_query(nil)]).html_safe
   end
 
   def self.transparent_gif
