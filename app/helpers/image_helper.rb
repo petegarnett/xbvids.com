a9173bab2155e27e06bf2176ea536cd93a83f45a
Nick Jones <nick@nicksays.co.uk>
2014-11-14 16:19:06 +0000

    Use own image proxy

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,14 +1,13 @@
 module ImageHelper
   def self.resized(url, width, height=nil)
     api_params = {
-      :url   => url,
+      :image   => url,
       :width => width,
-      :key   => '3b8da2efff024d37856e11ab0878dfa9'
     }
 
     api_params[:height] = height unless height.nil?
 
-    return ("https://i.embed.ly/1/image/resize?%s" % [api_params.to_query(nil)]).html_safe
+    return ("https://img.xbvids.com?%s" % [api_params.to_query(nil)]).html_safe
   end
 
   def self.transparent_gif
