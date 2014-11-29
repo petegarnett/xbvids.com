f8e99ea3961d9bdc41ba645e03cd487ba3bc41f6
Nick Jones <nick@nicksays.co.uk>
2014-11-29 17:35:12 +0000

    Use image resizer proxy  instead of embedly

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,14 +1,13 @@
 module ImageHelper
   def self.resized(url, width, height=nil)
     api_params = {
-      :url   => url,
-      :width => width,
-      :key   => '3b8da2efff024d37856e11ab0878dfa9'
+      :image => url,
+      :width => width
     }
 
     api_params[:height] = height unless height.nil?
 
-    return ("https://i.embed.ly/1/image/resize?%s" % [api_params.to_query(nil)]).html_safe
+    return ("https://imgrszr.herokuapp.com/?%s" % [api_params.to_query(nil)]).html_safe
   end
 
   def self.transparent_gif
